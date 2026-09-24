4aafef8f7ed58da82d2d3c1996ca0b98194f8c10
jazaineam1 <60367187+jazaineam1@users.noreply.github.com>
2026-09-24 17:34:02 -0500

    S07: QA prohíbe recursos legacy adicionales

--- utils/build_session7_elasticsearch.py
+++ utils/build_session7_elasticsearch.py
@@ -56,6 +56,8 @@ FORBIDDEN_LEGACY = [
     ROOT / "assets" / "tutoriales" / "s07-laboratorio-guiado.html",
     ROOT / "assets" / "tutoriales" / "s07-tokenizer-lab.html",
     ROOT / "assets" / "tutoriales" / "s07-recursos.html",
+    ROOT / "assets" / "tutoriales" / "s07-despliegue-elasticsearch.html",
+    ROOT / "Cuadernos" / "7B_Despliegue_Elastic_Cloud.ipynb",
 ]
 
 
